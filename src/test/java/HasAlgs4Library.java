787d226bb7fda176ef0b64e7959666434d0abcca
Øystein Hiåsen <oystein.hiaasen@gmail.com>
2019-10-10 20:56:01 +0200

    Initial build configuration for using Algs4

---
+++ src/test/java/HasAlgs4Library.java
@@ -0,0 +1,10 @@
+import edu.princeton.cs.algs4.Out;
+import org.junit.jupiter.api.Test;
+
+public class HasAlgs4Library {
+    @Test
+    void useSomeStuff () {
+        Out out = new Out();
+        out.println("Hello!");
+    }
+}
