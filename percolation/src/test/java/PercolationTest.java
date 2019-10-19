c50bf7aa493509084df3ed4b5f3f32da3e3b3759
Øystein Hiåsen <oystein.hiaasen@gmail.com>
2019-10-19 09:54:31 +0200

    Refactor PercolationTest.
    This fixes a PMD problem found:
        - JUnit tests should not contain more than 1 assert(s).

--- percolation/src/test/java/PercolationTest.java
+++ percolation/src/test/java/PercolationTest.java
@@ -13,77 +13,136 @@ class PercolationTest {
     }
 
     @Test
-    void throwsIllegalArgumentExceptionInConstructor() {
-        assertThrows(
-                IllegalArgumentException.class,
-                () -> new Percolation(-1)
-                );
+    void Constructor_ThrowsIllegalArgumentException_IfNIsNegative() {
+        assertThrowsIllegalArgument(() -> new Percolation(-1));
     }
+
+    // open
     @Test
-    void throwsIllegalArgumentExceptionIfOutsideRange() {
+    void open_ThrowsIllegalArgumentException_IfRowLargerThanN() {
         Percolation percolation = new Percolation(10);
         assertThrowsIllegalArgument(() -> percolation.open(11, 1));
+    }
+
+    @Test
+    void open_ThrowsIllegalArgumentException_IfColLargerThanN() {
+        Percolation percolation = new Percolation(10);
         assertThrowsIllegalArgument(() -> percolation.open(1, 11));
+    }
+    @Test
+    void open_ThrowsIllegalArgumentException_IfRowIsZero() {
+        Percolation percolation = new Percolation(10);
         assertThrowsIllegalArgument(() -> percolation.open(0, 1));
+    }
+
+    @Test
+    void open_ThrowsIllegalArgumentException_IfColIsZero() {
+        Percolation percolation = new Percolation(10);
         assertThrowsIllegalArgument(() -> percolation.open(1, 0));
-
+    }
+    // isOpen
+    @Test
+    void isOpen_ThrowsIllegalArgumentException_IfRowLargerThanN() {
+        Percolation percolation = new Percolation(10);
         assertThrowsIllegalArgument(() -> percolation.isOpen(11, 1));
-        assertThrowsIllegalArgument(() -> percolation.isOpen(1, 11));
-        assertThrowsIllegalArgument(() -> percolation.isOpen(0, 1));
-        assertThrowsIllegalArgument(() -> percolation.isOpen(1, 0));
+    }
 
+    @Test
+    void isOpen_ThrowsIllegalArgumentException_IfColLargerThanN() {
+        Percolation percolation = new Percolation(10);
+        assertThrowsIllegalArgument(() -> percolation.isOpen(1, 11));
+    }
+    @Test
+    void isOpen_ThrowsIllegalArgumentException_IfRowIsZero() {
+        Percolation percolation = new Percolation(10);
+        assertThrowsIllegalArgument(() -> percolation.isOpen(0, 1));
+    }
+
+    @Test
+    void isOpen_ThrowsIllegalArgumentException_IfColIsZero() {
+        Percolation percolation = new Percolation(10);
+        assertThrowsIllegalArgument(() -> percolation.isOpen(1, 0));
+    }
+    // isFull
+    @Test
+    void isFull_ThrowsIllegalArgumentException_IfRowLargerThanN() {
+        Percolation percolation = new Percolation(10);
         assertThrowsIllegalArgument(() -> percolation.isFull(11, 1));
+    }
+
+    @Test
+    void isFull_ThrowsIllegalArgumentException_IfColLargerThanN() {
+        Percolation percolation = new Percolation(10);
         assertThrowsIllegalArgument(() -> percolation.isFull(1, 11));
+    }
+    @Test
+    void isFull_ThrowsIllegalArgumentException_IfRowIsZero() {
+        Percolation percolation = new Percolation(10);
         assertThrowsIllegalArgument(() -> percolation.isFull(0, 1));
+    }
+
+    @Test
+    void isFull_ThrowsIllegalArgumentException_IfColIsZero() {
+        Percolation percolation = new Percolation(10);
         assertThrowsIllegalArgument(() -> percolation.isFull(1, 0));
     }
+
     @Test
-    void testOpen() {
+    void When_NewPercolation_Expect_AllSitesClosed() {
+        final int n = 10;
+        Percolation percolation = new Percolation(n);
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                assertFalse(percolation.isOpen(row, col));
+            }
+        }
+    }
+
+    @Test
+    void numberOfSites_IsZero_IfNewlyCreatedPercolation() {
         Percolation percolation = new Percolation(10);
-
-        assertFalse(percolation.isOpen(1,1));
-        assertEquals(percolation.numberOfOpenSites(), 0);
-
-        percolation.open(1,1);
+        assertEquals(0, percolation.numberOfOpenSites());
+    }
+    @Test
+    void numberOfSites_IsOne_AfterOpeningOneSite() {
+        Percolation percolation = new Percolation(10);
+        percolation.open(1, 1);
+        assertEquals(1, percolation.numberOfOpenSites());
+    }
+    @Test
+    void isOpen_True_AfterOpeningTheSite() {
+        Percolation percolation = new Percolation(10);
+        percolation.open(1, 1);
         assertTrue(percolation.isOpen(1,1));
-        assertEquals(percolation.numberOfOpenSites(), 1);
     }
     @Test
-    void noOpenedDoesNotPercolate() {
+    void percolates_false_IfNoOpenedSites() {
         Percolation percolation = new Percolation(10);
         assertFalse(percolation.percolates());
     }
 
     @Test
-    void singleSite() {
+    void Should_percolate_When_OpenedSiteInSystemWithOnlyOneSite() {
         Percolation percolation = new Percolation(1);
-        assertFalse(percolation.percolates());
-
         percolation.open(1,1);
         assertTrue(percolation.percolates());
     }
 
     @Test
-    void twoByTwo() {
+    void Should_percolate_When_PathOpenedInTwoByTwoSystem() {
         Percolation percolation = new Percolation(2);
-        assertFalse(percolation.percolates());
-
         percolation.open(1,1);
         percolation.open(2,2);
-        assertFalse(percolation.percolates());
-
         percolation.open(1,2);
+
         assertTrue(percolation.percolates());
     }
 
     @Test
-    void threeByThree() {
+    void Should_percolate_When_PathOpenedInThreeByThreeSystem() {
         Percolation percolation = new Percolation(3);
-        assertFalse(percolation.percolates());
-        percolation.open(2,1);
-        assertFalse(percolation.isFull(2, 1));
         percolation.open(1,1);
-        assertTrue(percolation.isFull(2, 1));
+        percolation.open(2,1);
         percolation.open(2,2);
         percolation.open(2,3);
         percolation.open(3,3);
@@ -91,19 +150,13 @@ class PercolationTest {
     }
 
     @Test
-    void backwash() {
+    void When_Percolates_DontBackwash() {
         // Test for recreating the backwash-bug
         Percolation p = new Percolation(3);
         p.open(1,1);
         p.open(2, 1);
         p.open(3,1);
-        assertTrue(p.percolates());
-
         p.open(3,3);
         assertFalse(p.isFull(3,3));
-
-        p.open(2,3);
-        p.open(1, 3);
-        assertTrue(p.isFull(3,3));
     }
 }
