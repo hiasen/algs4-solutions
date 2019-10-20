c4d8f1948d4bc69fe8cd0c90680809f1df925840
Øystein Hiåsen <oystein.hiaasen@gmail.com>
2019-10-20 10:46:51 +0200

    Fix checkstyle problems in percolation module.

--- percolation/src/main/java/Percolation.java
+++ percolation/src/main/java/Percolation.java
@@ -1,30 +1,40 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 /**
- * Implements the Percolation data type.
+ * Models a percolation system.
+ * <p>The model contains a grid of sites which can be either open or blocked.</p>
  */
 public class Percolation {
 
     private static final int TOP_SITE = 0;
     private static final int BOTTOM_SITE = 1;
 
-    private final int n;
+    private final int sideLength;
     private final WeightedQuickUnionUF uf;
     private final WeightedQuickUnionUF ufWithoutBottomSite;
     private final boolean[][] opened;
     private int numOpenedSites = 0;
 
+    /**
+     * Creates n-by-n grid, with all sites initially blocked.
+     * @param n Number of rows and columns.
+     */
     public Percolation(int n) {
         if (n <= 0) {
             throw new IllegalArgumentException();
         }
-        this.n = n;
+        this.sideLength = n;
         uf = new WeightedQuickUnionUF(n * n + 2);
         ufWithoutBottomSite = new WeightedQuickUnionUF(n * n + 2);
 
         opened = new boolean[n][n];
     }
 
+    /**
+     * Open a site.
+     * @param row The row of the site.
+     * @param col The column of the site.
+     */
     public void open(int row, int col) {
         if (!isOpen(row, col)) {
             opened[row - 1][col - 1] = true;
@@ -32,6 +42,13 @@ public class Percolation {
             unionNeighborSites(row, col);
         }
     }
+
+    /**
+     * Check if a site has been previously opened.
+     * @param row The row of the site.
+     * @param col The column of the site.
+     * @return true if site is open, false if blocked.
+     */
     public boolean isOpen(int row, int col) {
         checkRange(row, col);
         return opened[row - 1][col - 1];
@@ -42,8 +59,9 @@ public class Percolation {
             throw new IllegalArgumentException();
         }
     }
+
     private boolean isInRange(int row, int col) {
-        return row >= 1 && row <= n && col >= 1 && col <= n;
+        return row >= 1 && row <= sideLength && col >= 1 && col <= sideLength;
     }
 
     private void unionNeighborSites(int row, int col) {
@@ -52,7 +70,7 @@ public class Percolation {
             uf.union(site, TOP_SITE);
             ufWithoutBottomSite.union(site, TOP_SITE);
         }
-        if (row == n) {
+        if (row == sideLength) {
             uf.union(site, BOTTOM_SITE);
         }
         unionIfInRangeAndOpen(site, row - 1, col);
@@ -60,23 +78,45 @@ public class Percolation {
         unionIfInRangeAndOpen(site, row, col - 1);
         unionIfInRangeAndOpen(site, row, col + 1);
     }
+
     private void unionIfInRangeAndOpen(int site, int row, int col) {
         if (isInRange(row, col) && isOpen(row, col)) {
             uf.union(site, getSite(row, col));
             ufWithoutBottomSite.union(site, getSite(row, col));
         }
     }
+
     private int getSite(int row, int col) {
         checkRange(row, col);
-        return (row - 1) * n + col + 1;
+        return (row - 1) * sideLength + col + 1;
     }
 
+    /**
+     * Check if site is full.
+     *
+     *  <p>A full site is an open site that can be connected to an open site in the top row
+     *  via a chain of neighboring (left, right, up, down) open sites.</p>
+     * @param row The row of the site.
+     * @param col The column of the site.
+     * @return true if site is full.
+     */
     public boolean isFull(int row, int col) {
         return ufWithoutBottomSite.connected(TOP_SITE, getSite(row, col));
     }
+
+    /**
+     * Return the number of opened sites.
+     * @return Number of opened sites.
+     */
     public int numberOfOpenSites() {
         return numOpenedSites;
     }
+
+    /**
+     * Check if system percolates.
+     * The system percolates if there is a full site in the bottom row.
+     * @return true if system percolates.
+     */
     public boolean percolates() {
         return uf.connected(TOP_SITE, BOTTOM_SITE);
     }
--- percolation/src/main/java/PercolationStats.java
+++ percolation/src/main/java/PercolationStats.java
@@ -2,41 +2,60 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
-/** Runs an experiment to estimate percolation probability.
- *
+/**
+ * Represents a Monte Carlo simulation used to estimate the percolation threshold
+ * in a percolation system.
+ * <p>
+ *     The simulation is carried out by creating a number of trials as independent Percolation objects of equal size.
+ *     In each trial, random sites are opened until the system percolates,
+ *     and the number of opened sites when the percolation happens are saved.
+ * </p>
+ * <p>
+ *     The mean value and standard deviation of the percolation threshold is also computed.
+ * </p>
  */
 public class PercolationStats {
     private static final double CONFIDENCE_95 = 1.96;
-    private final int n;
+    private final int sideLength;
     private final int trials;
-    private final double xbar;
-    private final double s;
+    private final double meanValue;
+    private final double standardDeviation;
 
+    /**
+     * Creates and runs the simulation.
+     * @param n The number of rows and columns in each percolation grid.
+     * @param trials The number of independent trials to do.
+     */
     public PercolationStats(int n, int trials) {
         if (n <= 0 || trials <= 0) {
             throw new IllegalArgumentException();
         }
-        this.n = n;
+        this.sideLength = n;
         this.trials = trials;
         int[] results = new int[trials];
         for (int i = 0; i < trials; i++) {
             results[i] = doTrial();
         }
 
-        xbar = StdStats.mean(results)/(n*n);
-        s = StdStats.stddev(results)/(n*n);
+        meanValue = StdStats.mean(results) / (n * n);
+        standardDeviation = StdStats.stddev(results) / (n * n);
     }
 
     private int doTrial() {
-        Percolation p = new Percolation(n);
+        Percolation p = new Percolation(sideLength);
         while (!p.percolates()) {
-            int row = StdRandom.uniform(n) + 1;
-            int col = StdRandom.uniform(n) + 1;
+            int row = StdRandom.uniform(sideLength) + 1;
+            int col = StdRandom.uniform(sideLength) + 1;
             p.open(row, col);
         }
         return p.numberOfOpenSites();
     }
 
+
+    /**
+     * Runs the simulation and prints the results.
+     * @param args {n, trials}
+     */
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         int trials = Integer.parseInt(args[1]);
@@ -46,19 +65,35 @@ public class PercolationStats {
         StdOut.printf("95%% confidence interval = [%f, %f]\n", ps.confidenceLo(), ps.confidenceHi());
     }
 
+    /**
+     * Return computed mean value of percolation threshold.
+     * @return Mean value of percolation threshold.
+     */
     public double mean() {
-        return xbar;
+        return meanValue;
     }
 
+    /**
+     * Return computed standard deviation of percolation threshold.
+     * @return Standard deviation of percolation threshold.
+     */
     public double stddev() {
-        return s;
+        return standardDeviation;
     }
 
+    /**
+     * Lower bound of 95% confidence interval for percolation threshold.
+     * @return Lower bound of confidence interval.
+     */
     public double confidenceLo() {
-        return xbar - CONFIDENCE_95*s/Math.sqrt(trials);
+        return meanValue - CONFIDENCE_95 * standardDeviation / Math.sqrt(trials);
     }
 
+    /**
+     * Upper bound of 95% confidence interval for percolation threshold.
+     * @return Upper bound of confidence interval.
+     */
     public double confidenceHi() {
-        return xbar + CONFIDENCE_95*s/Math.sqrt(trials);
+        return meanValue + CONFIDENCE_95 * standardDeviation / Math.sqrt(trials);
     }
 }
